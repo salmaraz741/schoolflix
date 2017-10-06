904a9df275564183d8338cf1aa275d809076360f
salmaraz741 <salmaraz741@gmail.com>
2017-10-06 19:13:25 +0000

    Listing the courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
